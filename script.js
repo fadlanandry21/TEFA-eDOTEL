e302c4647418430652a1dca90aca55d21d2d0a56
fadlanandry21 <andryalfarisfadlan@gmail.com>
2024-10-27 20:04:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 150,
+    modifier: 2.5,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+});
